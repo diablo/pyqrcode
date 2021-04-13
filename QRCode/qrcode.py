3c134e8bacd66fe7c610cb0ede30519b6b8e895c
Corvo <snailteamsixunderground@gmail.com>
2021-04-13 04:48:12 +0100

    Add files via upload

---
+++ QRCode/qrcode.py
@@ -0,0 +1,13 @@
+import pyqrcode
+
+# .PNG
+source = "www.github.com/CorvoDev/"
+url = pyqrcode.create(source)
+url.png('qrcode.png', scale = 8)
+url.show()
+
+# .SVG
+# source = "www.github.com/CorvoDev/"
+# url = pyqrcode.create(source)
+# url.svg('qrcode.svg', scale = 8)
+# url.show()
